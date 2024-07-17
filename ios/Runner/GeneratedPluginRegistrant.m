145c2a376ebb811f270ec51f0f0f5795e669e6b3
netanel-yerushalmi <Netanel.y@sency.ai>
2024-07-17 15:36:11 +0300

    add sounds url

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -18,11 +18,18 @@
 @import integration_test;
 #endif
 
+#if __has_include(<path_provider_foundation/PathProviderPlugin.h>)
+#import <path_provider_foundation/PathProviderPlugin.h>
+#else
+@import path_provider_foundation;
+#endif
+
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
   [SMKitUIFlutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"SMKitUIFlutterPlugin"]];
   [IntegrationTestPlugin registerWithRegistrar:[registry registrarForPlugin:@"IntegrationTestPlugin"]];
+  [PathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"PathProviderPlugin"]];
 }
 
 @end
